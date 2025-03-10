a370e03a06a41168104b392becffa7279a5cb4a7
BarshaRani Sahoo1176 <147238995+Sahoobarsharani@users.noreply.github.com>
2025-03-10 10:57:44 +0530

    Add files via upload

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,1 +1,26 @@
+export const formatCurrency = (amount) => {
+  return new Intl.NumberFormat('en-IN', {
+    style: 'currency',
+    currency: 'INR'
+  }).format(amount);
+};
 
+export const formatDate = (date) => {
+  return new Date(date).toLocaleDateString('en-IN', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  });
+};
+
+export const calculateStats = (transactions) => {
+  return transactions.reduce((acc, transaction) => {
+    if (transaction.type === 'income') {
+      acc.income += transaction.amount;
+    } else {
+      acc.expenses += transaction.amount;
+    }
+    acc.balance = acc.income - acc.expenses;
+    return acc;
+  }, { income: 0, expenses: 0, balance: 0 });
+}; 
